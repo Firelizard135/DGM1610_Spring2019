bff4785b1fd99693d048c35bda827fb19e968140
Micah <hager.micah@gmail.com>
2019-02-06 17:13:26 -0700

    Created Player Controller

---
+++ 2D_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)) {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
+
+/*
+GetKey = keyboard_check()
+GetKeyDown = keyboard_check_pressed
+*/
