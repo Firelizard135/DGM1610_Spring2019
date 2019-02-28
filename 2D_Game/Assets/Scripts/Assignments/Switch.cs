53b4db76d2f9f71dbd206f744d89eb7e4780cfc4
Micah <hager.micah@gmail.com>
2019-02-27 17:07:19 -0700

    Switches

---
+++ 2D_Game/Assets/Scripts/Assignments/Switch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour{
+
+    public string suspect;
+
+
+    void Start() {
+        MurderMystery(suspect);
+    }
+
+    
+    void MurderMystery (string person) {
+        switch(person) {
+
+            case "Mr. Ketchup":
+            case "Mr. Radish":
+                print("I was in the billiard room drinking tea with Mr. Ketchup.");
+            break;
+            case "Ms. Frysauce":
+                print("I was talking on the rotary phone with my mother, Mrs. Mayo.");
+            break;
+            case "Mrs. Mayo":
+                print("I was in the kitchen cleaning the dishes.");
+            break;
+            default:
+                print("I am not familiar with "+suspect+" .");
+            break;
+        }
+    }
+}
+
+
