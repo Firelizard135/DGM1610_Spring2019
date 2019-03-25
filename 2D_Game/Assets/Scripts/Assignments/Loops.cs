4e9a80f9d6dcfe252e442662c1dfadc014d855e4
Micah <hager.micah@gmail.com>
2019-03-25 17:27:54 -0600

    Enemy AI

---
+++ 2D_Game/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    public int bottles = 1;
+    public bool isRaining = true;
+
+    void Start()
+    {
+        //For Loop
+        // for(int i=100; i > bottles; i--); {
+        //     print(i+" bottles of beer on the wall");
+        // }
+
+        // while(bottles <= 100){
+        //     print(i+" bottles of beer on the wall");
+        //     bottles++;
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D_Game/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    // Movement Variables
+    public float moveSpeed;
+    public bool moveRight;
+
+    //Wall Check
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool hittingWall;
+
+    //Edge Check
+    private bool notAtEdge;
+    public Transform edgeCheck;
+
+
+    void Update()
+    {
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+
+        if (hittingWall || !notAtEdge){
+            moveRight = !moveRight;
+        }
+
+        //Moving right or left
+        if(moveRight){
+            transform.localScale = new Vector3(-1f,1f,1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else {
+            transform.localScale = new Vector3(1f,1f,1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
