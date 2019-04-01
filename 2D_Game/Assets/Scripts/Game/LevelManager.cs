9242eda77608df8560fba711fa7834351d8aec49
Micah <hager.micah@gmail.com>
2019-04-01 17:16:02 -0600

    Kill player and level manager

--- 2D_Game/Assets/Scripts/Game/LevelManager.cs
+++ 2D_Game/Assets/Scripts/Game/LevelManager.cs
@@ -32,6 +32,37 @@ public class LevelManager : MonoBehaviour
         player = GameObject.Find("Player");
     }
 
+    public void RespawnPlayer() {
+        StartCoroutine("RespawnPlayerCo");
+    }
+
+    public IEnumerator RespawnPlayerCo(){
+        //generate death particle
+        Instantiate(deathParticle, pcRigid.transform.position, pcRigid.transform.rotation);
+        //Hide PC
+        player.SetActive(false);
+        pcRigid.GetComponent<Renderer> ().enabled = false;
+        //gravity reset
+        gravityStore = pcRigid.GetComponent<Rigidbody2D>().gravityScale;
+        pcRigid.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        pcRigid.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        //point penalty
+        ScoreManager.AddPoints(-pointPenaltyOnDeath);
+        // debug message
+        Debug.Log("PC Respawn");
+        //respawn delay
+        yield return new WaitForSeconds (respawnDelay);
+        //gravity restore
+        pcRigid.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
+        //match PCs transform position
+        pcRigid.transform.position = currentCheckPoint.transform.position;
+        //show pc
+        player.SetActive(true);
+        pcRigid.GetComponent<Renderer>().enabled = true;
+        //spawn PC
+        Instantiate(respawnParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
+    }
+
     // Update is called once per frame
     void Update()
     {
