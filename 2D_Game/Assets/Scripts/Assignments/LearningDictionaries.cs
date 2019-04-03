7e1a574ca7a216ff862e26ea396b6a6126a20c77
Micah <hager.micah@gmail.com>
2019-04-03 17:14:45 -0600

    Camera Follow Script, Destroy finished particles

---
+++ 2D_Game/Assets/Scripts/Assignments/LearningDictionaries.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningDictionaries : MonoBehaviour
+{
+
+    public Hashtable personalDetails = new Hashtable();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        personalDetails.Add("firstName", "Greg");
+        personalDetails.Add("lastName", "Lukosek");
+        personalDetails.Add("gender", "male");
+        personalDetails.Add("isMarried", "true");
+        personalDetails.Add("age", "29");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
