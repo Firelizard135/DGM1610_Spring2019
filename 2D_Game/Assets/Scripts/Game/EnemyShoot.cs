781fe73fcd3e4c3a0e14a2704026d962a758017f
Micah <hager.micah@gmail.com>
2019-04-26 13:10:00 -0600

    Enemy Shooting Script (not done yet)
    Started a script to make enemies shoot. Can sense player proximity and start a shooting coroutine.

---
+++ 2D_Game/Assets/Scripts/Game/EnemyShoot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+
+    public GameObject player;
+    private Collider2D playerCollider;
+
+    private Collider2D collider;
+
+    public float range;
+    public float shotDelay;
+    private float playerDistance;
+    private bool canShoot;
+
+    void Start()
+    {
+        playerCollider = player.GetComponent<Collider2D>();
+        collider = GetComponent<Collider2D>();
+        canShoot = true;
+    }
+
+    void Update()
+    {
+        playerDistance = Vector2.Distance(player.transform.position, transform.position);
+
+        if(playerDistance < range) 
+            if(canShoot == true) {
+                Shoot();
+            }
+    }
+
+    void Shoot()
+    {
+        StartCoroutine("ShootPlayer");
+        canShoot = false;
+    }
+
+    public IEnumerator ShootPlayer(){
+        print("enemy shoot");
+        yield return new WaitForSeconds (shotDelay);
+        canShoot = true;
+    }
+}
+
