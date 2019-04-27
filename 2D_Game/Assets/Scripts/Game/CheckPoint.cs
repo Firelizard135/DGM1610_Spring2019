006e1783cf238929e343912cc2c0f06ed846cde8
Micah <hager.micah@gmail.com>
2019-04-27 12:20:08 -0600

    Finished Level Layout + Springboard
    Also made a new coin asset to match the pixel art.

---
+++ 2D_Game/Assets/Scripts/Game/CheckPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public GameObject levelManager;
+    private LevelManager levelManagerScript;
+
+    void Start() {
+        levelManagerScript = levelManager.GetComponent<LevelManager>();
+    }
+
+    // Hits checkpoint
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.name == "Player") {
+            levelManagerScript.currentCheckPoint = gameObject;
+        }
+    }
+}
---
+++ 2D_Game/Assets/Scripts/Game/SpringBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringBoard : MonoBehaviour
+{
+    public Animator animator;
+    public float springForce;
+
+    void Start(){
+        animator.SetBool("isTriggered",false);
+    }
+
+    // Press Button
+    void OnTriggerEnter2D(Collider2D other) {
+        animator.SetBool("isTriggered",true);
+        other.GetComponent<Rigidbody2D>().velocity = new Vector2(other.GetComponent<Rigidbody2D>().velocity.x, 0);
+        other.GetComponent<Rigidbody2D>().AddForce(transform.up*springForce*100);
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        animator.SetBool("isTriggered",false);
+    }
+}
